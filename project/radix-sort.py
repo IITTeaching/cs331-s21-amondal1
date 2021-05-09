3fae3be2708ad96c8a6ae4f5ca371eb424d9c489
abhee <abhee@104.194.119.246>
2021-05-09 17:25:07 -0500

    project-stillworkingonit

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -11,12 +11,37 @@ def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     strng = padding(text.split())
     return strng 
     
+def radix_sort(s,swap,longest):
+    max = longest(s)
+    for x in range (max-1,0,-1):
+        for val in range(len(s)):
+            for e in range(val,len(s)):
+                try:
+                    if (s[val][x]>s[val+1][x]):
+                        swap(s[val],s[val+1])
+                except:
+                    pass
+    return s
+
+def swap (s,x,y):
+    s[x],s[y] = s[y], s[x]
+    return s
+    
+def longest(s):
+    max = 0
+    for i in s:
+        if(len(s)>max):
+            max = len(s)
+    return max
+    
 
 def main():
-    book = radix_a_book()
-    for w in book:
-        print(w)
-        
+    s = book_to_words()
+    print(s[0:10])
+    print("test")
+    
+    
+   #book = radix_a_book()
         
         
 if __name__ == '__main__':
